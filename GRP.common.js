38b925cdfbd70f5cb9430d277ca26201844b3cf8
DaveLomber <admin@connectycube.com>
2022-04-11 14:50:47 +0300

    removed bluebird deps

--- GRP.common.js
+++ GRP.common.js
@@ -8,10 +8,22 @@ if (typeof self === 'undefined') {
 }
 
 var GRP = require('react-native').NativeModules.GRP;
-var NativeAppEventEmitter = require('react-native').NativeAppEventEmitter;
-var Promise = require('bluebird');
 
-var _getRealPathFromURI = Promise.promisify(GRP ? GRP.getRealPathFromURI : (fileUri) => { return fileUri; });
+function promisify(func) {
+  return function promiseFunc(options) {
+      return new Promise(function executor(resolve, reject) {
+          func(options, function cb(err, val) {
+              if (err) {
+                  return reject(err);
+              } else {
+                  return resolve(val);
+              }
+          });
+      });
+  }
+}
+
+var _getRealPathFromURI = promisify(GRP ? GRP.getRealPathFromURI : (fileUri) => { return fileUri; });
 
 var convertError = (err) => {
   if (err.isOperational && err.cause) {
